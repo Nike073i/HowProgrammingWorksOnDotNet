aff6707f8e1d1af0ebfdc0134f7087db5e2bfa1f
Nike073i <nike073i@mail.ru>
2025-05-14 15:07:06 +0400

    Container for theory data
    Вспомогательный класс для создания данных к тестам в виде ClassData

---
+++ TestUtils/TestData/TheoryDataContainer.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+
+namespace HowProgrammingWorksOnDotNet.TestUtils.TheoryData
+{
+    /*
+        Author Idea - Andrew Lock.
+        Modified by Skuld
+        Link - https://andrewlock.net/creating-strongly-typed-xunit-theory-test-data-with-theorydata/
+    */
+    public abstract class TheoryDataContainer : IEnumerable<object?[]>
+    {
+        private TheoryDataContainer() { }
+
+        private readonly List<object?[]> _dataset = [];
+
+        private void AddRow(params object?[] args) => _dataset.Add(args);
+
+        public IEnumerator<object?[]> GetEnumerator() => _dataset.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        public abstract class OneArg<T> : TheoryDataContainer
+        {
+            protected void Add(T arg) => AddRow(arg);
+        }
+
+        public abstract class TwoArg<T1, T2> : TheoryDataContainer
+        {
+            protected void Add(T1 t1, T2 t2) => AddRow(t1, t2);
+        }
+
+        public abstract class ThreeArg<T1, T2, T3> : TheoryDataContainer
+        {
+            protected void Add(T1 t1, T2 t2, T3 t3) => AddRow(t1, t2, t3);
+        }
+    }
+}
