6a6608f26be5fa75ad2202a4a82cd0a0814927e6
Nike073i <nike073i@mail.ru>
2025-05-17 00:21:57 +0400

    incorrect implementation Money

--- ObjectOrientedProgramming/ValueObjects/Money.cs
+++ ObjectOrientedProgramming/ValueObjects/Money.cs
@@ -1,63 +1,65 @@
-namespace HowProgrammingWorksOnDotNet.ObjectOrientedProgramming.ValueObjects;
+// Некорректная реализация. PositveMoney создает базовый объект Money минуя все проверки
 
-public record Money
-{
-    public decimal Amount { get; }
-    public string Currency { get; }
+// namespace HowProgrammingWorksOnDotNet.ObjectOrientedProgramming.ValueObjects;
 
-    // Internal защищает от несанкционированного доступа к конструктору вне метода Create через наследование.
-    // Унаследовать в обход Create можно только в том-же проекте. В другом проекте для наследования доступен лишь конструктор копирования от Record
-    internal Money(decimal amount, string currency)
-    {
-        Amount = amount;
-        Currency = currency;
-    }
+// public record Money
+// {
+//     public decimal Amount { get; }
+//     public string Currency { get; }
 
-    public static Money Create(decimal amount, string currency = "Default")
-    {
-        // Something check...
-        return new Money(amount, currency);
-    }
+//     // Internal защищает от несанкционированного доступа к конструктору вне метода Create через наследование.
+//     // Унаследовать в обход Create можно только в том-же проекте. В другом проекте для наследования доступен лишь конструктор копирования от Record
+//     internal Money(decimal amount, string currency)
+//     {
+//         Amount = amount;
+//         Currency = currency;
+//     }
 
-    public static Money operator +(Money first, Money second)
-    {
-        if (first.Currency != second.Currency)
-            throw new InvalidOperationException("Currencies have to be equal");
-        // or return first with { Amount = first.Amount + second.Amount };
-        return Create(first.Amount + second.Amount, first.Currency);
-    }
+//     public static Money Create(decimal amount, string currency = "Default")
+//     {
+//         // Something check...
+//         return new Money(amount, currency);
+//     }
 
-    public static implicit operator Money(decimal amount) => Create(amount);
-}
+//     public static Money operator +(Money first, Money second)
+//     {
+//         if (first.Currency != second.Currency)
+//             throw new InvalidOperationException("Currencies have to be equal");
+//         // or return first with { Amount = first.Amount + second.Amount };
+//         return Create(first.Amount + second.Amount, first.Currency);
+//     }
 
-public record PositiveMoney : Money
-{
-    private PositiveMoney(decimal Amount, string Currency)
-        : base(Amount, Currency) { }
+//     public static implicit operator Money(decimal amount) => Create(amount);
+// }
 
-    public static new PositiveMoney Create(decimal Amount, string Currency = "Default")
-    {
-        if (Amount < 0)
-            throw new ArgumentException("Amount must be positive");
-        return new PositiveMoney(Amount, Currency);
-    }
+// public record PositiveMoney : Money
+// {
+//     private PositiveMoney(decimal Amount, string Currency)
+//         : base(Amount, Currency) { }
 
-    public static implicit operator PositiveMoney(decimal amount) => Create(amount);
-}
+//     public static new PositiveMoney Create(decimal Amount, string Currency = "Default")
+//     {
+//         if (Amount < 0)
+//             throw new ArgumentException("Amount must be positive");
+//         return new PositiveMoney(Amount, Currency);
+//     }
 
-public class MoneyTests
-{
-    [Fact]
-    public void MoneyOperations()
-    {
-        Money vova = -15;
-        Assert.Throws<ArgumentException>(() =>
-        {
-            PositiveMoney error = -15;
-        });
-        PositiveMoney nikita = 15;
+//     public static implicit operator PositiveMoney(decimal amount) => Create(amount);
+// }
 
-        Money balance = nikita + vova;
-        Assert.Equal(0, balance.Amount);
-    }
-}
+// public class MoneyTests
+// {
+//     [Fact]
+//     public void MoneyOperations()
+//     {
+//         Money vova = -15;
+//         Assert.Throws<ArgumentException>(() =>
+//         {
+//             PositiveMoney error = -15;
+//         });
+//         PositiveMoney nikita = 15;
+
+//         Money balance = nikita + vova;
+//         Assert.Equal(0, balance.Amount);
+//     }
+// }
