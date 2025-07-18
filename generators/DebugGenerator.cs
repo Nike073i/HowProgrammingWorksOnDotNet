d945b22e3e2582eb90d9181589d2c518a37e27a5
Nike073i <Nike073i@mail.ru>
2025-07-18 12:32:14 +0400

    Добавление проекта с генераторами

---
+++ generators/DebugGenerator.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace generators;
+
+[Generator]
+public class DebugGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        context.RegisterPostInitializationOutput(context =>
+        {
+            context.AddSource("debug.g.cs", "// debug-file");
+        });
+    }
+}
