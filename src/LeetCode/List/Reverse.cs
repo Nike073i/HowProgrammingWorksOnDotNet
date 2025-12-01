cb44b5ac6bcb4fce10bcd9f366314bf94dd6d019
Nike073i <Nike073i@mail.ru>
2025-12-01 23:37:11 +0400

    add: LeetCode 206 - Reverse list inplace

---
+++ src/LeetCode/List/Reverse.cs
@@ -0,0 +1,29 @@
+namespace HowProgrammingWorksOnDotNet.LeetCode.List;
+
+/*
+    leetcode: 206 https://leetcode.com/problems/reverse-linked-list/description/
+    time: O(n)
+    memory: O(1)
+*/
+public class ListNode(int val = 0, ListNode next = null)
+{
+    public int val = val;
+    public ListNode next = next;
+}
+
+public class Solution
+{
+    public ListNode ReverseList(ListNode head)
+    {
+        ListNode? prev = null;
+        ListNode curr = head;
+        while (curr != null)
+        {
+            var next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
